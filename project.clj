b9c513bb6eab55565aead8ec43d7a6bf3fb2756c
tony-o <tony.odell@live.com>
2021-02-19 15:38:18 -0800

    s/SNAPSHOT//

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject uri-escape "0.0.1-SNAPSHOT"
-  :description "uri percent encoder written in clojure"
-  :url "https://github.com/tony-o/clj-uri-encoder"
+(defproject uri-escape "0.0.1"
+  :description "uri percent encoder/decoder written in clojure"
+  :url "https://github.com/tony-o/clj-uri-escape"
   :license "Artistic-2.0"
   :dependencies [[org.clojure/clojure "1.10.1"]]
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]]
 )
